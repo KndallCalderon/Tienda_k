3ab69984d3a21e729ebe7e9df2742748923f1148
calde <calde@PC-KNDALL>
2022-01-31 17:28:59 -0600

    Primer commit del proyecto Tienda_k

---
+++ src/main/java/com/Tienda_k/TiendaKApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaKApplication.class, args);
+	}
+
+}
