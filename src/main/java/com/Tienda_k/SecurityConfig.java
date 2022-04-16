2fceeaf5022cd160750eb79afb1dd6619cf0c20e
calde <calde@PC-KNDALL>
2022-04-16 17:28:19 -0600

    Clase 10

--- src/main/java/com/Tienda_k/SecurityConfig.java
+++ src/main/java/com/Tienda_k/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.Tienda_k;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.util.AntPathMatcher;
 
 /**
  *
@@ -27,5 +29,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .withUser("usuario")
                 .password("(noop)123")
                 .roles("USER");                
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/articulo/nuevo", "/articulo/guardar",
+                        "/articulo/modificar/**", "/articulo/eliminar/**",
+                        "/categoria/nuevo", "/categoria/guardar",
+                        "/categoria/modificar/**", "/categoria/eliminar/**",
+                        "/cliente/nuevo", "/cliente/guardar",
+                        "/cliente/modificar/**", "/cliente/eliminar/**",
+                        "/usuario/nuevo", "/usuario/guardar",
+                        "/usuario/modificar/**", "/usuario/eliminar/**"
+                ).hasRole("ADMIN")
+                .antMatchers("/articulo/listado", "/categoria/listado", "/cliente/listado")
+                .hasAnyRole("ADMIN", "VENDEDOR")
+                .antMatchers("/")
+                .hasAnyRole("ADMIN", "VENDEDOR", "USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/errores/403");
     }
 }
